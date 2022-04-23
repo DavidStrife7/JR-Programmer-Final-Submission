472b6a46970256571d2b7012ec59a8b2568fd5b4
DavidStrife7 <51286862+DavidStrife7@users.noreply.github.com>
2022-04-23 23:17:42 +0900

    Created Default Ball Class
    Default Ball is ready with virtual functions to override later

--- Assets/BallDefault.cs
+++ Assets/BallDefault.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class BallDefault : MonoBehaviour
 {
-    private Color;
+
+    private Transform thisBall;
+    private void Start()
+    {
+        thisBall = GetComponent<Transform>();
+    }
+    public virtual void MoveUp()
+    {
+        thisBall.position += (Vector3.up * Time.deltaTime);
+    }
+
+    public virtual void MoveDown()
+    {
+        thisBall.position += (Vector3.down * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("UpArrow"))
+        {
+            MoveUp();
+        }
+
+        if (Input.GetKeyDown("UpArrow"))
+        {
+            MoveDown();
+        }
+    }
 }
